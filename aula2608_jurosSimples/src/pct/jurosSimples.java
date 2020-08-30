8cc1bd72aca6a3eb621d65c333a951fbf6de1e5b
Vitor Assis <Vitor Assis@DESKTOP-GI2M2BV>
2020-08-29 22:56:44 -0300

    Adição de juros

--- aula2608_jurosSimples/src/pct/jurosSimples.java
+++ aula2608_jurosSimples/src/pct/jurosSimples.java
@@ -25,49 +25,78 @@ public class jurosSimples {
 
         // commite João Vitor Pereira
         do {
-            //apresentação
-            System.out.println("\n\t\t\t -- Calculadora de Juros Simples -- \n");
+                //apresentação
+                System.out.println("\n\t\t\t -- Calculadora de Juros Simples -- \n");
 
-            //Menu
-            System.out.println("O que você deseja calcular?");
-            System.out.println("[1] - Capital");
-            System.out.println("[2] - juros");
-            System.out.println("[3] - Taxa");
-            System.out.println("[4] - Tempo");
-            System.out.println("[5] - Sair");
+                //Menu
+                System.out.println("O que você deseja calcular?");
+                System.out.println("[1] - Capital");
+                System.out.println("[2] - Juros");
+                System.out.println("[3] - Taxa");
+                System.out.println("[4] - Tempo");
+                System.out.println("[5] - Sair");
 
-            //opção escolhida
-            op = Integer.parseInt(entrada.nextLine());
+                //opção escolhida
+                op = Integer.parseInt(entrada.nextLine());
 
-            if (op == 1) {
-                System.out.println("Digite o Juros");
-                juros = Float.parseFloat(entrada.nextLine());
+                if (op == 1) {
+                    System.out.println("Digite o Juros");
+                    juros = Float.parseFloat(entrada.nextLine());
 
-                System.out.println("Digite a taxa");
-                taxa = Float.parseFloat(entrada.nextLine());
+                    System.out.println("Digite a taxa");
+                    taxa = Float.parseFloat(entrada.nextLine());
 
-                System.out.println("Digite o tempo");
-                tempo = Float.parseFloat(entrada.nextLine());
+                    System.out.println("Digite o tempo");
+                    tempo = Float.parseFloat(entrada.nextLine());
 
-                //transformando tempo de dias para (meses)
-                if (tempo >= 30) {
-                    //para não precisar criar outra variavel, usei a mesma anterior 
-                    tempo = tempo / 30;
+                    //transformando tempo de dias para (meses)
+                    if (tempo >= 30) {
+                        //para não precisar criar outra variavel, usei a mesma anterior 
+                        tempo = tempo / 30;
+                    }
+                    //multiplicando tempo e taxa
+                    float tempTax = tempo * taxa;
+
+                    juros = juros * 100;
+
+                    capital = juros / tempTax;
+
+                    System.out.println("o Capital é de " + capital);
+
+                    //juros
+
+                } else if (op == 2) {
+                    System.out.println("Digite a Capital");
+                    capital = Float.parseFloat(entrada.nextLine());
+
+                    System.out.println("Digite a taxa");
+                    taxa = Float.parseFloat(entrada.nextLine());
+
+                    System.out.println("Digite o tempo");
+                    tempo = Float.parseFloat(entrada.nextLine());
+
+                    //transformando tempo de dias para (meses)
+                    if (tempo >= 30) {
+                        //para não precisar criar outra variavel, usei a mesma anterior 
+                        tempo = tempo / 30;
+                    }
+                    //transformando porcentual em indice
+                    taxa = taxa / 100;
+
+                    juros = capital * tempo * taxa;
+
+                    System.out.println("O seu Juros é de: " + juros);
+                     } 
+
+                else if (op == 5) {
+                  System.out.println("Obrigado, até a próxima!");
                 }
-                //multiplicando tempo e taxa
-                float tempTax = tempo * taxa;
+                else{             
+                   System.out.println("O nnumero digitado não corresponde a nenhuma das opções.");
+                    }
+        } while(op != 5);
 
-                juros = juros * 100;
-
-                capital = juros / tempTax;
-
-                System.out.println("o Capital é de " + capital);
-            } else if (op == 5) {
-                System.out.println("Obrigado, até a próxima!");
-            }
-
-        } while (op != 5);
-
+        }
     }
 
-}
+
